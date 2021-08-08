8ab0345a104e3aee2f182a3dbaff07fbc32e3a47
ig0xb <ig0xb@protonmail.com>
2021-08-08 16:59:29 +0530

    gigasecond

---
+++ gigasecond/src/gigasecond.c
@@ -0,0 +1,5 @@
+#include "gigasecond.h"
+
+time_t gigasecond_after(time_t t) {
+    return t + pow(10, 9);
+}
---
+++ gigasecond/src/gigasecond.h
@@ -0,0 +1,8 @@
+#ifndef GIGASECOND_H
+#define GIGASECOND_H
+
+#include <math.h>
+#include <time.h>
+time_t gigasecond_after(time_t);
+
+#endif
